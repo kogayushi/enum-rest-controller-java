bc1fb217bc74f13ff98a7315190011fcc1e76a97
kogayushi <youshe.old.pleasure@gmail.com>
2019-11-17 22:44:44 +0900

    first commit

---
+++ src/main/java/com/example/enumrestcontrollerjava/EnumRestControllerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.enumrestcontrollerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumRestControllerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnumRestControllerJavaApplication.class, args);
+	}
+
+}
